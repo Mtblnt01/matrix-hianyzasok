b7724621d54ae1f2b5ec03e6027821c974b8006e
1tc-borpet <126462737+1tc-borpet@users.noreply.github.com>
2023-03-06 09:25:42 +0100

    fajlbeolvasas

---
+++ megoldas.py
@@ -0,0 +1,12 @@
+#0.feladat A hianyzasok.txt beolvasa listaban listakba
+
+hianyzasok=[]
+with open("./adatok/hianyzasok.txt","r",encoding="UTF-8") as fm:
+    for sor in fm:
+        seged_lista=sor.strip().split(",")
+        # l=[]
+        # for szam in seged_lista:
+        #     l.append(int(szam))
+        # hianyzasok.append(l)
+        hianyzasok.append(list(map(int, seged_lista)))
+print(hianyzasok)
